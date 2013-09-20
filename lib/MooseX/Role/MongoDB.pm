62f3e40c0c549ea24dc69aebee3d2f4b46448d61
David Golden <dagolden@cpan.org>
2013-09-20 16:55:16 -0400

    initial import

---
+++ lib/MooseX/Role/MongoDB.pm
@@ -0,0 +1,34 @@
+use v5.10;
+use strict;
+use warnings;
+
+package MooseX::Role::MongoDB;
+# ABSTRACT: No abstract given for MooseX::Role::MongoDB
+# VERSION
+
+
+1;
+
+=for Pod::Coverage BUILD
+
+=head1 SYNOPSIS
+
+    use MooseX::Role::MongoDB;
+
+=head1 DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+=head1 USAGE
+
+Good luck!
+
+=head1 SEE ALSO
+
+=for :list
+* Maybe other modules do related things.
+
+=cut
+
+# vim: ts=4 sts=4 sw=4 et:
