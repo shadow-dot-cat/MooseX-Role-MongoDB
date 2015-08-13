2c27bcbeb536f4d9669299a8fa8d2ecc32c8a329
David Golden <dagolden@cpan.org>
2015-08-13 17:53:29 -0400

    After release: bump $VERSION and timestamp Changes

--- lib/MooseX/Role/MongoDB.pm
+++ lib/MooseX/Role/MongoDB.pm
@@ -5,7 +5,7 @@ use warnings;
 package MooseX::Role::MongoDB;
 # ABSTRACT: Provide MongoDB connections, databases and collections
 
-our $VERSION = '0.009';
+our $VERSION = '0.010';
 
 use Moose::Role 2;
 use MooseX::AttributeShortcuts;
